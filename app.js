7e6fba26cb6283fb89dcaa831b5493b69cf0434e
GaloDev <bagxxi@gmail.com>
2025-03-02 12:51:20 -0300

    Enhance friend addition and drawing logic; improve CSS layout and responsiveness

--- app.js
+++ app.js
@@ -22,18 +22,19 @@ function limpiar() {
 // Función para agregar un amigo a la lista
 function agregarAmigo() {
     let nombre = document.getElementById('amigo').value;
-    nombre = nombre[0].toUpperCase() + nombre.slice(1).toLowerCase();
-    if (nombre == '') {
+    if (nombre.length == 0) {
         modalText.textContent = "Creo que olvidaste escribir el nombre de tu amigo";
         mostrarModal();
     }
     else{
+        nombre = nombre[0].toUpperCase() + nombre.slice(1).toLowerCase();
         if (amigos.includes(nombre)) {
             modalText.textContent = nombre + " ya está en la lista, intenta con un nombre diferente o pensaremos que te quedaste sin amigos";
             mostrarModal();
             return;
         }
         else {
+            nombre = nombre[0].toUpperCase() + nombre.slice(1).toLowerCase();
             amigos.push(nombre);
             limpiar();
             console.log(amigos);
@@ -58,6 +59,16 @@ function mostrarAmigos() {
 function sortearAmigo() {
     if (amigos.length === 0) {
         modalText.textContent = "No puedes iniciar el sorteo sin amigos. Por favor, agrega amigos a la lista";
+        mostrarModal();
+        return;
+    }
+    else if (amigos.length === 1) {
+        modalText.textContent = "No puedes sortear a un amigo si solo hay uno en la lista, qué sad";
+        mostrarModal();
+        return;
+    }
+    else if (amigos.length < 3) {
+        modalText.textContent = "No puedes sortear a un amigo si hay menos de 3 en la lista, ponéle voluntad";
         mostrarModal();
         return;
     }
